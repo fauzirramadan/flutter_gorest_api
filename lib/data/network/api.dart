d726d427ee5a1a2ce2b3412f100560924cbc3bc7
fauzirramadan <fauzirramadan@gmail.com>
2023-02-02 20:16:47 +0800

    hide api key

--- lib/data/network/api.dart
+++ lib/data/network/api.dart
@@ -1,10 +1,9 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Api {
-  static final authorization = Options(headers: {
-    "Authorization":
-        "Bearer 65bce06e8de5bf2ecd5b76cbab7676b9aca47f78e77c1c25c91f8b76acf5a607"
-  });
+  static final authorization =
+      Options(headers: {"Authorization": "Bearer ${dotenv.env['Api_Key']}"});
 
   static const String baseUrl = "https://gorest.co.in/public/v2";
   static const String getUserApi = "$baseUrl/users";
