c39dfacb2498183e688e51bdd0b73c0e1892e7e4
fauzirramadan <fauzirramadan@gmail.com>
2023-02-02 18:26:54 +0800

    add repository

--- lib/data/network/api.dart
+++ lib/data/network/api.dart
@@ -8,4 +8,5 @@ class Api {
 
   static const String baseUrl = "https://gorest.co.in/public/v2";
   static const String getUserApi = "$baseUrl/users";
+  static String deleteUserApi(int id) => "$getUserApi/$id";
 }
--- lib/data/repository/general_repo.dart
+++ lib/data/repository/general_repo.dart
@@ -1,1 +1,70 @@
-class GeneralRepo {}
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_gorest_api/data/helper/either.dart';
+import 'package:flutter_gorest_api/data/helper/failure.dart';
+import 'package:flutter_gorest_api/data/model/user.dart';
+import 'package:flutter_gorest_api/data/network/api.dart';
+import 'package:flutter_gorest_api/data/network/dio_helper.dart';
+import 'package:flutter_gorest_api/data/response/res_get_user.dart';
+
+class GeneralRepo {
+  Future<Either<Failure, List<ResGetUser>>> fetchListUser(int page) async {
+    try {
+      Response res = await dio.get(Api.getUserApi,
+          options: Api.authorization,
+          queryParameters: {"per_page": 15, "page": page});
+      return Either.success(resGetUserFromJson(res.data));
+    } catch (e, st) {
+      if (kDebugMode) {
+        log(st.toString());
+      }
+      return Either.error(Failure(e.toString()));
+    }
+  }
+
+  Future<Either<Failure, User>> addOrUpdateUser(
+      {String? name,
+      String? email,
+      String? gender,
+      String? status,
+      bool isUpdate = false}) async {
+    try {
+      Response res = !isUpdate
+          ? await dio.post(Api.getUserApi, options: Api.authorization, data: {
+              "name": name,
+              "email": email,
+              "gender": gender,
+              "status": status
+            })
+          : await dio.put(Api.getUserApi, options: Api.authorization, data: {
+              "name": name,
+              "email": email,
+              "gender": gender,
+              "status": status
+            });
+      return Either.success(userFromJson(res.data));
+    } catch (e, st) {
+      if (kDebugMode) {
+        log(st.toString());
+      }
+      return Either.error(Failure(e.toString()));
+    }
+  }
+
+  Future<Response?> deleteUser(int id) async {
+    try {
+      Response res =
+          await dio.delete(Api.deleteUserApi(id), options: Api.authorization);
+      return res;
+    } catch (e, st) {
+      if (kDebugMode) {
+        log(st.toString());
+      } else {
+        log(e.toString());
+      }
+    }
+    return null;
+  }
+}
